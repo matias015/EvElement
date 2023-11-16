e28b5fc57acd79ccac419f06f64e2dcf92a9a48b
matias015 <78274208+matias015@users.noreply.github.com>
2023-11-16 14:06:42 -0300

    Update ElementEv.js

--- ElementEv.js
+++ ElementEv.js
@@ -1,368 +1,1039 @@
 class ElementEv {
 
+
+
   constructor(tag, exists) {
+
     if (exists) {
+
       this.element = tag
+
     }
+
     else if (tag) {
+
       this.element = document.createElement(tag)
+
     } else {
+
       this.element = null
+
     }
+
     this.lastEventSetted = null
 
+
+
     this.prevSib = null
+
     this.nextSib = null
 
+
+
     this.lastElementCreated = null
+
     this.fragment = document.createDocumentFragment()
 
+
+
     this.interceptObject = null
+
   }
 
+
+
   create(tag) {
+
     this.element = document.createElement(tag)
+
     return this
+
   }
+
+
+
 
 
   // SEARCHING
 
-  inParent(parent){
-    if(typeof parent == "string") parent = document.querySelector(parent)
-    else if(parent instanceof ElementEv) parent = parent.get()
+
+
+  inParent(parent) {
+
+    if (typeof parent == "string") parent = document.querySelector(parent)
+
+    else if (parent instanceof ElementEv) parent = parent.get()
+
     this.parentSearch = parent
+
     return this
+
   }
+
+
+
+
+
+
 
   whereClass(clss, cb) {
-    if(this.parentSearch) this.element = this.parentSearch.querySelector('.' + clss)
-    else this.element = document.querySelector('.' + clss)
-    
-    if (cb && !this.element) {
-      this.element = cb()
+
+    if (clss instanceof Array) {
+
+      clss = __ElementEv__multiFilterConcat__(clss, '.')
+
     }
-    this.parentSearch=null;
+
+    if (this.parentSearch) this.element = this.parentSearch.querySelector('.' + clss)
+
+    else this.element = document.querySelector('.' + clss)
+
+
+
+    if (cb && !this.element) {
+
+      this.element = cb()
+
+    }
+
+    this.parentSearch = null;
+
     return this
+
   }
+
+
 
   whereId(id, cb) {
-    if(this.parentSearch) this.element = this.parentSearch.querySelector('#' + id)
-    else this.element = document.querySelector('#' + id)
-    if (cb && !this.element) {
-      this.element = cb()
+
+    if (id instanceof Array) {
+
+      id = __ElementEv__multiFilterConcat__(clss, '#')
+
     }
-    this.parentSearch=null;
+
+    if (this.parentSearch) this.element = this.parentSearch.querySelector('#' + id)
+
+    else this.element = document.querySelector('#' + id)
+
+    if (cb && !this.element) {
+
+      this.element = cb()
+
+    }
+
+    this.parentSearch = null;
+
     return this
+
   }
+
+
 
   whereTag(tag, cb) {
-    if(this.parentSearch) this.element = this.parentSearch.querySelector(tag)
+
+    if (tag instanceof Array) {
+
+      tag = __ElementEv__multiFilterConcat__(clss, '')
+
+    }
+
+    if (this.parentSearch) this.element = this.parentSearch.querySelector(tag)
+
     else this.element = document.querySelector(tag)
 
+
+
     if (cb && !this.element) {
+
       this.element = cb()
+
     }
-    this.parentSearch=null;
+
+    this.parentSearch = null;
+
     return this
+
   }
+
+
 
   whereProp(values, cb) {
-    if(this.parentSearch) this.element = this.parentSearch.querySelector(`[${values[0]}=${values[1]}]`)
+
+    if (this.parentSearch) this.element = this.parentSearch.querySelector(`[${values[0]}=${values[1]}]`)
+
     else this.element = document.querySelector(`[${values[0]}=${values[1]}]`)
 
+
+
     if (cb && !this.element) {
+
       this.element = cb()
+
     }
-    this.parentSearch=null;
+
+    this.parentSearch = null;
+
     return this
+
   }
+
+
+
+
 
 
 
   // EVENTS
-  when(ev) {
+
+  when(ev,cb) {
+
     this.lastEventSetted = ev
+
     return this
+
+    if(cb) this.make(cb)
+
   }
+
+
 
   make(cb) {
+
+    let el = this
+
     this.element.addEventListener(this.lastEventSetted, function(e) {
-      e.element = new ElementEv(e.target, true)
+
+      e.element = el
+
       cb(e)
+
     })
+
     return this
+
   }
 
+
+
   toggle(el, clss) {
+
     const e = document.querySelector(el)
+
     this.make(function() {
+
       e.classList.toggle(clss)
+
     })
+
     return this
+
   }
+
+
+
+  propToggle(el, values) {
+
+    this.make(function() {
+
+      if (el.attrIs(values[0], values[1])) {
+
+        el.attr(values[0], values[2])
+
+      } else {
+
+        el.attr(values[0], values[1])
+
+      }
+
+    })
+
+    return this
+
+  }
+
+
 
   interception(cb, obj) {
 
+
+
     var r,
+
       margin,
+
       th,
+
       cb2
 
+
+
     if (!obj) obj = {}
+
     if (!obj.whilenot) cb2 = false
+
     else cb2 = true
 
+
+
     r = obj.root || document
+
     if (typeof r == 'string') r = document.querySelector(r)
 
+
+
     if (!obj.delay) margin = '0px'
+
     else margin = `${obj.delay * 1.5}px 0px -${obj.delay * 1.5}px 0px`
+
     th = obj.th || 0.5
 
+
+
     this.interceptObject = new IntersectionObserver(function(ent) {
+
       ent.forEach(e => {
+
         if (e.isIntersecting) cb()
+
         else {
+
           if (cb2) obj.whilenot()
+
         }
+
       })
+
     }, { root: r, rootMargin: margin, threshold: th })
 
+
+
     this.interceptObject.observe(this.element)
+
     return this
+
   }
 
+
+
   endInterception() {
+
     this.interceptObject.unobserve(this.element)
+
     return this
+
   }
+
+
+
 
 
   // CONTENT
-  setContent(c) {
+
+  setText(c) {
+
     this.element.textContent = c
+
     return this
+
   }
 
-  contentIs(str) {
+
+
+  textIs(str) {
+
     return this.element.textContent == str
+
   }
+
+
 
   clear() {
+
     this.element.innerHTML = ''
+
     return this
+
   }
+
+
 
   // CHILDS and parents
+
   createChild(tag) {
+
     if (!this.fragment) {
+
       this.fragment = document.createDocumentFragment()
+
     }
+
+
 
     if (this.lastElementCreated) {
+
       this.fragment.appendChild(this.lastElementCreated.element)
+
     }
+
     if (tag instanceof ElementEv) {
+
       this.lastElementCreated = tag
+
     } else {
+
       this.lastElementCreated = new ElementEv(tag)
+
     }
+
     return this
+
   }
 
-  withContent(c) {
+
+
+  withText(c) {
+
     this.lastElementCreated.setContent(c)
+
     return this
+
   }
+
+
 
   insert() {
+
     if (this.lastElementCreated) {
+
       this.fragment.appendChild(this.lastElementCreated.element)
+
       this.lastElementCreated = null
+
     }
+
     this.element.appendChild(this.fragment)
+
     this.fragment = null
+
     return this
+
   }
+
+
 
   appendChild(child) {
+
     //  console.log('insert');
+
     this.element.appendChild(child.get())
+
     return this
+
   }
+
+
 
   withChildren(nodes) {
+
     if (nodes instanceof Array) {
+
       const thisCopy = this
+
       nodes.forEach(function(element) {
+
         thisCopy.fragment.appendChild(element.get())
+
       })
 
+
+
       thisCopy.element.appendChild(this.fragment)
+
       this.fragment = null
+
       return thisCopy
+
     }
 
+
+
     this.insert(nodes)
+
     return this
+
   }
+
+
+
 
 
   lastChild() {
+
     return new ElementEv(this.element.children[this.element.children.length - 1], true)
+
   }
+
+
 
   firstChild() {
+
     return new ElementEv(this.element.children[0], true)
+
   }
+
+
 
   parent(deep) {
+
     if (!deep) return new ElementEv(this.element.parentElement, true)
 
+
+
     let parent = this.element.parentElement;
+
     for (var i = 1; i < deep; i++) {
+
       parent = parent.parentElement
+
     }
+
+
 
     return new ElementEv(parent, true)
+
   }
+
+
 
   hasNextSibling() {
+
     if (this.get().nextSibling) return true
+
     else return false
+
   }
+
+
 
   hasPrevSibling() {
+
     if (this.get().prevSibling) return true
+
     else return false
+
   }
+
+
 
   prevSibling(deep) {
+
     if (!deep) return new ElementEv(this.element.previousSibling, true)
 
+
+
     let sib = this.element.previousSibling;
+
     for (var i = 1; i < deep; i++) {
+
       sib = sib.previousSibling
+
     }
 
+
+
     return new ElementEv(sib, true)
+
   }
+
+
 
   nextSibling(deep) {
+
     if (!deep) return new ElementEv(this.element.nextSibling, true)
 
+
+
     let sib = this.element.nextSibling;
+
     for (var i = 1; i < deep; i++) {
+
       sib = sib.nextSibling
+
     }
 
+
+
     return new ElementEv(sib, true)
+
   }
+
+
+
 
 
   // ELEMENT
+
   get() {
+
     return this.element
+
   }
 
+
+
   value() {
+
     field = this.element
+
     if (field.type === 'checkbox') {
-      return field.checked? field.value : null;
+
+      return field.checked ? field.value : null;
+
     } else if (field.type === 'radio') {
-      return field.checked? field.value : null;
+
+      return field.checked ? field.value : null;
+
     } else {
+
       return field.value;
+
     }
+
   }
+
+
+
 
 
   valueIs(val) {
+
     return this.element.value == val
+
   }
+
+
 
   tagIs(tag) {
+
     return this.element.tagName == tag.toUpperCase()
+
   }
+
+
 
   remove() {
+
     this.element.parentElement.removeChild(this.element)
+
     return this
+
   }
 
+
+
   move(to) {
+
     let el = this.get()
+
     this.remove()
+
     to.element.appendChild(el)
+
     return this
+
   }
+
+
+
+  removeAfter(ms) {
+
+    let el = this
+
+    let timer = setTimeout(function() {
+
+      el.remove()
+
+    }, ms)
+
+    return this
+
+  }
+
+
+
+  toggleAfter(ms, clss) {
+
+    let el = this
+
+
+
+    let timer = setTimeout(function() {
+
+
+
+      el.element.classList.toggle(clss)
+
+    }, ms)
+
+    return this
+
+  }
+
 
 
   // CLASSES AND PROPS
 
+
+
   hasClass(clss) {
+
     return this.element.classList.contains(clss)
+
   }
 
+  
+
+  addClass(c){
+
+    this.element.classList.add(c)
+
+    return this
+
+  }
+
+  
+
+  removeClass(c) {
+
+    this.element.classList.remove(c)
+
+    return this
+
+  }
+
+  
+
+  toggleClass(c) {
+
+    this.element.classList.toggle(c)
+
+    return this
+
+  }
+
+
+
   withAttrs(pairs) {
+
     let copy = this
+
+
 
     if (this.lastElementCreated) {
 
+
+
       Object.entries(pairs).forEach(function(pair) {
+
         copy.lastElementCreated.setAttribute(pair[0], pair[1])
+
       })
+
+
 
     } else {
 
+
+
       Object.entries(pairs).forEach(function(pair) {
+
         copy.element.setAttribute(pair[0], pair[1])
+
       })
 
+
+
     }
+
     return copy
+
   }
 
-  prop(name){
+
+
+  prop(name) {
+
     return this.element[name]
+
   }
+
+
+
+  attr(key, value) {
+
+    if (!value) return this.element.getAttribute(key)
+
+    else this.element.setAttribute(key, value)
+
+    return this
+
+  }
+
+
+
+  attrIs(key, value) {
+
+    return this.element.getAttribute(key) == value
+
+  }
+
+
+
+  position() {
+
+    let rect = this.element.getBoundingClientRect();
+
+    return { 'top': rect.top, 'right': rect.right, 'bottom': rect.bottom, 'left': rect.left }
+
+  }
+
+
 
   // Others
 
+
+
   print() {
+
     console.log(this.element.outerHTML);
+
     return this
+
   }
+
+
 
   style(css) {
+
     var el = this.element
+
     Object.entries(css).forEach(function(p) {
+
       el.style[p[0]] = p[1]
+
     })
+
     //this.element = el
+
     return this
+
   }
 
 
+
+  hide() {
+
+    this.element.style.display = 'none'
+
+    return this
+
+  }
+
+
+
+  show(val) {
+
+    if (!val) val = 'block'
+
+    this.element.style.display = val
+
+    return this
+
+  }
+
+
+
+  visible(opacity) {
+
+    this.element.style.opacity = opacity
+
+    return this
+
+  }
+
+
+
+
+
+  draggable() {
+
+    this.attr('draggable', true)
+
+
+
+    const elemento = this.element
+
+    let offsetX, offsetY;
+
+    let isDragging = false;
+
+
+
+    elemento.addEventListener('mousedown', (e) => {
+
+      isDragging = true;
+
+      offsetX = e.clientX - elemento.getBoundingClientRect().left;
+
+      offsetY = e.clientY - elemento.getBoundingClientRect().top;
+
+    });
+
+
+
+    document.addEventListener('mousemove', (e) => {
+
+      if (isDragging) {
+
+        elemento.style.left = `${e.clientX - offsetX}px`;
+
+        elemento.style.top = `${e.clientY - offsetY}px`;
+
+      }
+
+    });
+
+
+
+    document.addEventListener('mouseup', () => {
+
+      isDragging = false;
+
+    });
+
+  }
+
+  
+
+  touchDraggable() {
+
+    const elemento = this.element
+
+    let offsetX, offsetY;
+
+    let isDragging = false;
+
+
+
+    elemento.addEventListener('touchstart', (e) => {
+
+      isDragging = true;
+
+      const touch = e.touches[0];
+
+      offsetX = touch.clientX - elemento.getBoundingClientRect().left;
+
+      offsetY = touch.clientY - elemento.getBoundingClientRect().top;
+
+    });
+
+
+
+    document.addEventListener('touchmove', (e) => {
+
+      if (isDragging) {
+
+        const touch = e.touches[0];
+
+        elemento.style.left = `${touch.clientX - offsetX}px`;
+
+        elemento.style.top = `${touch.clientY - offsetY}px`;
+
+      }
+
+    });
+
+
+
+    document.addEventListener('touchend', () => {
+
+      isDragging = false;
+
+    });
+
+
+
+  }
+
+
+
 }
+
+
 
 function element(tag) {
+
   if (tag instanceof HTMLElement) {
+
     return new ElementEv(tag, true)
+
   }
+
   return new ElementEv(tag)
+
 }
 
+
+
 function create(tag, content) {
+
   tag = tag.replace(">", '') //for <
+
   tag = tag.replace('<', '')
+
+
+
 
 
   element = new ElementEv(tag)
 
+
+
   if (content) element.setContent(content)
 
+
+
   return element
+
 }
 
+
+
+
+
+// privates
+
+
+
+function __ElementEv__multiFilterConcat__(list, prefix) {
+
+
+
+  let multi = ""
+
+
+
+  for (let id of list) {
+
+    multi += id + "," + prefix;
+
+  }
+
+
+
+  return multi.substr(0, multi.length - 2)
+
+
+
+  }
