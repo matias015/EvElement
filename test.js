b932bdc52f34da955e09132b3c74e7a312de8246
matias015 <78274208+matias015@users.noreply.github.com>
2023-12-14 20:46:57 -0600

    Update test.js

--- test.js
+++ test.js
@@ -1,62 +1,10 @@
-const evButton = _find('#ev')
-// const cloneButton = _find('#clone')
-const newButton = _find('#new')
-
-const iter = 30000;
-
-// cloneButton.when('click', function(){
-//     const ul = _find('ul')
-
-//     ul.createChild('<li>')
-//         .withClasses('misma_clase')
-//         .withAttrs({'name':'mismo_name'})
-
-//     for(let i=0;i<=iter;i++){
-//         console.log(2);
-//        ul.cloneLast().withAttrs({'value':i}).withText(i)
-//     }
-
-//     ul.insert()
-// })
-
-evButton.when('click', function(){
-    const ul = _find('ul')
-
-    ul.createChild('li').withChildren([
-        create('a').attr('href','https://www.google.com').setText('google'),
-        create('a').attr('href','https://www.youtube.com/').setText('youtube'),
-    ]).withStyle({
-        'display':'flex',
-        'flexDirection':'column'
-    })
-
-    ul.insert()
-})
+let testElement;
 
 
-newButton.when('click', function(){
-   
-})
+testElement = new ElementEv('.test-ul');
+console.log(testElement);
 
-setTimeout(function(){
-    console.log('start');
-    let value = _find('input').value()
-    console.log('value is: ' + value);
+let loaded = document.querySelector('.test-ul');
+testElement = new ElementEv(loaded);
+console.log(testElement);
 
-    if(value==1){
-       
-        let ul = _find('ul')
-
-        element().whereTag('li',function(){
-            console.log(32);
-            return create('<li>','this content').insertInto(ul)
-        })
-
-    }else if(value==2){
-
-        // case 2   
-
-    }
-
-}, 3000)
-    
